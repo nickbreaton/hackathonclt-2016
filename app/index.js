42c3de9d0867e577514d028935742f7edda1a8b9
Nick Breaton <nick@breaton.com>
2016-03-19 01:01:49 -0400

    Get sponsers

--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
 window.addEventListener('load', function () {
+   var level = 0;
+   var index = 0;
+
    var ref = new Firebase("https://hackathonclt2016.firebaseio.com/");
    var img = $('img');
+   var input = $('input[type="text"]')
 
-   ref.child('sponsers/coke').once('value', function (snapshot) {
-      snapshot.forEach(function (node) {
-         img.attr('src', node.val());
+   var sponsers = [];
+   ref.child('sponsers').once('value', function (data) {
+      data.forEach(function (sponser) {
+         sponsers.push(sponser.val());
       });
+      name();
    });
+
+   function name () {
+      console.log(sponsers);
+   }
 });
