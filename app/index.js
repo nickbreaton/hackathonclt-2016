e3f441e32efb693fe6142b6f44bfa83b7efcf617
Nick Breaton <nick@breaton.com>
2016-03-19 00:23:34 -0400

    Add firebase integration

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-// alert('hi');
+window.addEventListener('load', function () {
+   var ref = new Firebase("https://hackathonclt2016.firebaseio.com/");
+   var img = $('#logo');
+
+   ref.child('sponsers/coke').once('value', function (snapshot) {
+      snapshot.forEach(function (node) {
+         img.attr('src', node.val());
+      });
+   });
+});
